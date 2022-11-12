4c1293639f46154e07c47e02bd870af49c14ed3e
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-12 20:18:05 +0900

    Update TodoListViewController.swift

--- Todooz/TodoListViewController.swift
+++ Todooz/TodoListViewController.swift
@@ -15,6 +15,20 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         
     }
+    
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+
+        return cell
+    }
 
 
 }

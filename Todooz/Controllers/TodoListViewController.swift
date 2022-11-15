923fdafed42b189db23f1d51667bbba447a76c59
Eldor Alikuvvatov <eldoralikuvvatov@gmail.com>
2022-11-15 14:56:40 +0900

    Update TodoListViewController.swift

--- Todooz/Controllers/TodoListViewController.swift
+++ Todooz/Controllers/TodoListViewController.swift
@@ -46,6 +46,12 @@ class TodoListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        if itemArray[indexPath.row].done == true {
+            cell.accessoryType = .checkmark
+        } else {
+            cell.accessoryType = .none
+        }
 
         return cell
     }
@@ -54,14 +60,11 @@ class TodoListViewController: UITableViewController {
     //MARK - Tableview Delegate Methods
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+        //print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-        
+        itemArray[indexPath.row].done =  !itemArray[indexPath.row].done
+           
+        tableView.reloadData()
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
